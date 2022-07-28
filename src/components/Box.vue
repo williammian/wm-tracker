5ffd646ee873a6e04334225a60b7b29f516669bc
William Mian <william_mian@yahoo.com.br>
2022-07-28 14:47:48 -0300

    vue

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box has-text-weight-bold" :style="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "BoxTracker",
+    data() {
+        return {
+            estilos: {
+                background: '#FAF0CA'
+            }
+        }
+    }
+})
+
+</script>
