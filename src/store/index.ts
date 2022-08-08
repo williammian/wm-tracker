94667ed4f9db25d7b02b818d0781b2a46abd92db
William Mian <william_mian@yahoo.com.br>
2022-08-08 15:51:45 -0300

    organizando o estado projetos

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,48 +1,30 @@
-import { ALTERAR_PROJETO, CADASTRAR_PROJETO, OBTER_PROJETOS, REMOVER_PROJETO, OBTER_TAREFAS, CADASTRAR_TAREFA, ALTERAR_TAREFA } from './tipo-acoes';
+import { EstadoProjeto, projeto } from './modulos/projeto/index';
+import { OBTER_TAREFAS, CADASTRAR_TAREFA, ALTERAR_TAREFA } from './tipo-acoes';
 import { INotificacao } from './../interfaces/INotificacao';
-import IProjeto from "@/interfaces/IProjeto";
 import ITarefa from "@/interfaces/ITarefa";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR, REMOVE_TAREFA } from "./tipo-mutacoes";
+import { ADICIONA_TAREFA, ALTERA_TAREFA, DEFINIR_TAREFAS, NOTIFICAR, REMOVE_TAREFA } from "./tipo-mutacoes";
 import http from "@/http"
 
-interface State {
-    projetos: IProjeto[],
+export interface Estado {
     tarefas: ITarefa[],
-    notificacoes: INotificacao[]
+    notificacoes: INotificacao[],
+    projeto: EstadoProjeto
 }
 
-export const key: InjectionKey<Store<State>> = Symbol();
+export const key: InjectionKey<Store<Estado>> = Symbol();
 
-export const store = createStore<State>({
+export const store = createStore<Estado>({
     state: {
-        projetos: [],
         tarefas: [],
-        notificacoes: []
+        notificacoes: [],
+        projeto: {
+            projetos: []
+        }
     },
 
     mutations: {
-        //Projeto
-        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
-            const projeto = {
-                id: new Date().toISOString(),
-                nome: nomeDoProjeto
-            } as IProjeto;
-            state.projetos.push(projeto);
-        },
-        [ALTERA_PROJETO](state, projeto: IProjeto) {
-            const index = state.projetos.findIndex(proj => proj.id == projeto.id);
-            state.projetos[index] = projeto;
-        },
-        [EXCLUIR_PROJETO](state, id: string) {
-            state.projetos = state.projetos.filter(proj => proj.id != id);
-        },
-
-        [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
-            state.projetos = projetos;
-        },
-
         //Tarefa
         [ADICIONA_TAREFA](state, tarefa: ITarefa) {
             //tarefa.id = new Date().toISOString();
@@ -53,7 +35,7 @@ export const store = createStore<State>({
             state.tarefas[indice] = tarefa;
         },
         [REMOVE_TAREFA](state, id: string) {
-            state.projetos = state.projetos.filter(p => p.id != id);
+            state.tarefas = state.tarefas.filter(p => p.id != id);
         },
 
         [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
@@ -72,24 +54,6 @@ export const store = createStore<State>({
     },
 
     actions: {
-        //Projetos
-        [OBTER_PROJETOS]({ commit }) {
-            http.get('projetos')
-                .then(resposta => commit(DEFINIR_PROJETOS, resposta.data))
-        },
-        [CADASTRAR_PROJETO](contexto, nomeDoProjeto: string) {
-            return http.post('projetos', {
-                nome: nomeDoProjeto
-            })
-        },
-        [ALTERAR_PROJETO](contexto, projeto: IProjeto) {
-            return http.put(`projetos/${projeto.id}`, projeto)
-        },
-        [REMOVER_PROJETO]({ commit }, id: string) {
-            return http.delete(`projetos/${id}`)
-                .then(() => commit(EXCLUIR_PROJETO, id))
-        },
-
         //Tarefas
         [OBTER_TAREFAS]({ commit }) {
             http.get('tarefas')
@@ -103,9 +67,13 @@ export const store = createStore<State>({
             return http.put(`tarefas/${tarefa.id}`, tarefa)
                 .then(() => commit(ALTERA_TAREFA, tarefa))
         },
+    },
+
+    modules: {
+        projeto
     }
 })
 
-export function useStore(): Store<State> {
+export function useStore(): Store<Estado> {
     return vuexUseStore(key);
 }
